499c95cc296e2ffc8a208fcd25769c8e604a0fe0
guuyoog <707304262@qq.com>
2018-03-05 00:43:50 +0800

    commit

---
+++ src/main/java/com/anhuibaiyi/website/dto/ResultCode.java
@@ -0,0 +1,17 @@
+package com.anhuibaiyi.website.dto;
+
+/**
+ * @author guuyoog
+ * @date 2018/3/4
+ */
+public class ResultCode {
+
+    /**
+     * 成功
+     */
+    public static int SUCCESS_CODE = 200;
+    /**
+     * 失败
+     */
+    public static int ERROR_CODE = 500;
+}
